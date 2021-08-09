87db01c41ff5e407c16778b767e11904229bc5b9
15night <misa000524@gmail.com>
2021-08-09 23:15:30 +0900

    [add]ログイン処理

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
-import { GoogleLogin } from "./Google/GoogleLogin"
+import { GoogleLogin, GoogleLoginInterface } from "./Google/GoogleLogin"
+import { WebDriverFactory } from "./Chrome/ChromeWebDriver"
+import { DownloadFile } from "./Chrome/downloadFile"
 import { JobcanLogin } from "./Jobcan/JobcanLogin"
 import { getLogger } from "./Log/GetLogger"
+import config from "config"
+import path from "path"
 
 const log = getLogger()
 
 async function workFlow() {
-  //  TODOログイン処理
+  const downloadFileInstance = new DownloadFile(
+    "fileDownload",
+    path.join(process.cwd(), "fileDownload"),
+    new WebDriverFactory()
+  )
+
+  // ログイン処理
+  const JobcanLoginData: GoogleLoginInterface = {
+    driver: downloadFileInstance.driver,
+    id: config.get<string>("google.id"),
+    pw: config.get<string>("google.pw")
+  }
+  const jobcanUrl = config.get<string>("Jobcan.Url")
+
+  await new JobcanLogin(JobcanLoginData, jobcanUrl).login()
 }
 workFlow()
